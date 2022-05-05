d04296074a9238125ad9893c97cce3b4bce7a329
yc62897441 <yc62897441@gmail.com>
2022-05-05 14:09:52 +0800

    feat: handlebars && set db

--- app.js
+++ app.js
@@ -7,13 +7,33 @@ const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {

6f69744d34817e2356e247e3e4c81d62b67b0dcc
yc62897441 <yc62897441@gmail.com>
2022-05-06 13:20:59 +0800

    feat: todo-new page

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,11 +3,15 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-router.get('/:id', (req, res) => {
+router.get('/todo/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => console.log(error))
+})
+
+router.get('/new', (req, res) => {
+  res.render('new')
 })
 
 module.exports = router

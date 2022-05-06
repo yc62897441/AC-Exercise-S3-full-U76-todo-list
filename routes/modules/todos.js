bc38ddca736c1fcbe16aa50407ed531c412bf27f
yc62897441 <yc62897441@gmail.com>
2022-05-06 13:40:16 +0800

    feat: todo-new post

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -14,4 +14,16 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
+router.post('/', (req, res) => {
+  const name = req.body.name
+  
+  Todo.create({
+    name: name,
+    isDone: false,
+    UserId: req.user.id
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 module.exports = router

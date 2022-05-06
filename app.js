e7e431cf8e8c2aac9b0227086d4365e34e9c5f11
yc62897441 <yc62897441@gmail.com>
2022-05-06 11:01:16 +0800

    feat: build detail page

--- app.js
+++ app.js
@@ -25,6 +25,13 @@ app.get('/', (req, res) => {
     .catch((error) => { return res.status(422).json(error) })
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
